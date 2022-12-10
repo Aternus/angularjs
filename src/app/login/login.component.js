1408aab2a49520b3c0b381c6b2e07103a146bd26
Aternus <kiril@atern.us>
2022-12-10 14:12:04 +0200

    - converted loginController from a factory function to a Class

--- src/app/login/login.component.js
+++ src/app/login/login.component.js
@@ -1,14 +1,14 @@
 import angular from 'angular';
 import './login.style.scss';
-import {LoginControllerClass} from './login.controller';
+import LoginController from './login.controller';
 import template from './login.template.html';
 import appNavbarComponent from '../common/app-navbar/app-navbar.component';
 import appFooterComponent from '../common/app-footer/app-footer.component';
 
 export const componentName = 'login';
 export const componentOptions = {
-  bindings: LoginControllerClass.bindings,
-  controller: LoginControllerClass,
+  bindings: LoginController.bindings,
+  controller: LoginController,
   template
 };
 
--- src/app/login/login.controller.js
+++ src/app/login/login.controller.js
@@ -1,26 +1,4 @@
-function LoginController($scope, $location, AuthN) {
-  $scope.user = {
-    email: '',
-    password: ''
-  };
-  $scope.AuthN = AuthN;
-
-  $scope.onLoginSubmit = async function () {
-    if (await AuthN.login($scope.user)) {
-      $location.path('/');
-    }
-  };
-
-  $scope.$on('$destroy', function () {
-    AuthN.resetErrorMessages();
-  });
-}
-
-LoginController.bindings = {};
-
-export default LoginController;
-
-export class LoginControllerClass {
+export default class LoginController {
   static bindings = {};
 
   constructor($location, AuthN) {
